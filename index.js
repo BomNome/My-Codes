c36ce45ec53c9e8e49169197374fdf4b7dcef65b
Vitor <132232270+BomNome@users.noreply.github.com>
2023-05-17 16:22:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+//variaveis que armazenam os valores para calculo
+
+var a = 1
+var b = 2
+
+//operações
+
+function soma(a, b){
+    return a + b;
+}
+function multi(a, b){
+    return a * b;
+}
+function sub(a, b){
+    return a - b;
+}
+
+console.log("A soma dos números 1 e 2 é...");
+console.log(soma(a, b));
+
+console.log("\nA multiplicação dos números 1 e 2 é...");
+console.log(multi(a, b));
+
+console.log("\nA subtração dos números 1 e 2 é...");
+console.log(sub(a, b));
+
+
