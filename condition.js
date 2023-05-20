8848e4504e211a5669d64d942112b2ff894435db
Vitor <132232270+BomNome@users.noreply.github.com>
2023-05-20 19:41:44 -0300

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,15 @@
+var a = 30;
+
+function soma(a,b){
+    return a + b;
+}
+
+if(a>30){
+    console.log('Variavel "a" é maior que', 30);
+}
+else if(a<30){
+    console.log('Variavel "a" é menor que', 30);
+}
+else{
+    console.log('Variavel "a" é igual a', 30);
+}
